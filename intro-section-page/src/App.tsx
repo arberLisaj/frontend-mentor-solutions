0cf43cff974526ef1a090c775dc8a9e5eee75afb
arberlisaj <arberlisaj1@gmail.com>
2023-05-25 10:40:12 +0200

    toggle menu functionality

--- intro-section-page/src/App.tsx
+++ intro-section-page/src/App.tsx
@@ -1,9 +1,12 @@
 import Navbar from "./components/Navbar";
+import Home from "./components/Home";
+import "./styles/styles.css";
 
 const App = () => {
   return (
     <main>
       <Navbar />
+      <Home />
     </main>
   );
 };
