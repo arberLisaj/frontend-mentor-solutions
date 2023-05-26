59d699edbca6d469c103f25dc207ef01494810c0
arberlisaj <arberlisaj1@gmail.com>
2023-05-27 00:08:03 +0200

    newsletter form test

--- newsletter-sign-up-form/src/App.tsx
+++ newsletter-sign-up-form/src/App.tsx
@@ -1,6 +1,19 @@
+import Form from "./components/Form/Form";
 import "./styles/styles.css";
 const App = () => {
-  return <div>App </div>;
+  return (
+    <div>
+      <Form />
+    </div>
+  );
 };
 
 export default App;
+
+// const LoginSchema = z.object({
+//   email: z
+//     .string()
+//     .min(1, { message: "This field has to be filled." })
+//     .email("This is not a valid email.")
+//   })
+// });
